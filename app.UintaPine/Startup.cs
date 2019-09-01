64384725840a5178857c0d155b70a35afbdf2c35
Dahln Farnes <dahln@outlook.com>
2019-08-31 23:45:17 -0600

    Added AppState as Singleton

--- app.UintaPine/Startup.cs
+++ app.UintaPine/Startup.cs
@@ -1,3 +1,4 @@
+using app.UintaPine.Services;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,6 +8,7 @@ namespace app.UintaPine
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<AppState>();
         }
 
         public void Configure(IComponentsApplicationBuilder app)
