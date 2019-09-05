eadac7eaed4b6c50cfaaf5ef3c325c7a9edb3b0f
Dahln Farnes <dahln@outlook.com>
2019-09-05 15:08:11 -0600

    Replaced the app project with the original template from preview 9

--- app.UintaPine/Startup.cs
+++ app.UintaPine/Startup.cs
@@ -1,8 +1,5 @@
-using app.UintaPine.Services;
-using Microsoft.AspNetCore.Blazor.Http;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using System.Runtime.InteropServices;
 
 namespace app.UintaPine
 {
@@ -10,17 +7,10 @@ namespace app.UintaPine
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<AppState>();
         }
 
         public void Configure(IComponentsApplicationBuilder app)
         {
-            //https://github.com/aspnet/AspNetCore/issues/9894
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Create("WEBASSEMBLY")))
-            {
-                WebAssemblyHttpMessageHandler.DefaultCredentials = FetchCredentialsOption.Include;
-            }
-
             app.AddComponent<App>("app");
         }
     }
