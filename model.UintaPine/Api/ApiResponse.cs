fe9532033ed168f83c55f745cceac90431403489
Dahln Farnes <dahln@outlook.com>
2019-09-08 15:13:09 -0600

    Fixed the api response success to be true

--- model.UintaPine/Api/ApiResponse.cs
+++ model.UintaPine/Api/ApiResponse.cs
@@ -6,7 +6,7 @@ namespace model.UintaPine.Api
 {
     public class ApiResponse
     {
-        public bool Success { get; set; }
+        public bool Success { get; set; } = true;
         public string Message { get; set; }
     }
 }
