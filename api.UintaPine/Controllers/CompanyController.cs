be188f5c8f06f21d05c453abb1e116440a71746d
Dahln Farnes <dahln@outlook.com>
2019-09-19 14:13:01 -0600

    Added empty company controller for company api

---
+++ api.UintaPine/Controllers/CompanyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.UintaPine.Controllers
+{
+    public class CompanyController : ControllerBase
+    {
+        public CompanyController()
+        {
+
+        }
+    }
+}
