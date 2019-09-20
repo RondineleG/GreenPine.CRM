e3edc9f2479df4fa17f6d98c50d56eaa7dbccaf3
Dahln Farnes <dahln@outlook.com>
2019-09-20 11:27:59 -0600

    Check in before changing all namespaces

--- api.UintaPine/Controllers/CompanyController.cs
+++ api.UintaPine/Controllers/CompanyController.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using logic.UintaPine;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using model.Server.UintaPine;
 
 namespace api.UintaPine.Controllers
 {
     public class CompanyController : ControllerBase
     {
-        public CompanyController()
+        private CompanyLogic _companyLogic { get; set; }
+        private ApplicationSettings _applicationSettings { get; set; }
+        public CompanyController(CompanyLogic companyLogic, ApplicationSettings applicationSettings)
         {
-
+            _companyLogic = companyLogic;
+            _applicationSettings = applicationSettings;
         }
     }
 }
---
+++ logic.UintaPine/CompanyLogic.cs
@@ -0,0 +1,18 @@
+﻿using data.UintaPine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace logic.UintaPine
+{
+    public class CompanyLogic
+    {
+        private MongoContext _db { get; set; }
+        private UtilityLogic _utilityLogic { get; set; }
+        public CompanyLogic(MongoContext context, UtilityLogic utilityLogic)
+        {
+            _db = context;
+            _utilityLogic = utilityLogic;
+        }
+    }
+}
